1895a72b51bf76b0b9d52b5ac89fd59db29879f7
dbrembilla <davide.brembilla98@gmail.com>
2022-05-06 08:59:56 +0200

    dirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import argparse
-
+import os
 from os import sep
 import time
 from multithread_populating import populateJson
@@ -10,10 +10,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process files to populate with Crossref information about presence.')
     parser.add_argument('path', metavar='path',type=str, 
                     help='Path to the file or to the directory')
-   
+    
     args = parser.parse_args()
     start = time.time()
     print(f'Populating json files in {args.path}. \nStarting...')
+    os.makedirs(f'.{sep}temp')
+    os.makedirs(f'.{sep}temp{sep}completed')
     pop = populateJson()
     pop.populate(args.path)
     print(f'Finished in {time.time() - start} seconds')
